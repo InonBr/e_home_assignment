4746785dde8174a11e92f244b83548328b3ecc64
Inon Bercu <inonbr@gmail.com>
2021-08-02 16:28:13 +0300

    added aws url to react app (to make requests to)

--- twilio_react_app/src/lib/urls.js
+++ twilio_react_app/src/lib/urls.js
@@ -1,5 +1,4 @@
 const makeCallUrl =
-  'http://ec2-18-116-165-93.us-east-2.compute.amazonaws.com:5000/api/make_call' ||
-  'http://127.0.0.1:5000/api/make_call';
+  process.env.REACT_APP_BACK_END_URL || 'http://127.0.0.1:5000/api/make_call';
 
 export { makeCallUrl };
